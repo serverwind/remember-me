ade19410f1b4ae397cc77380c46e34e444b44f57
lestrangeqq <lestrangeqq@gmail.com>
2024-01-12 11:31:08 +0200

    Added package with icons - Boxicons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./style.css";
+import "boxicons";
 import "./components/loadApp";
 import "./components/select-proj";
-import "./components/new-proj"
-import "./components/remove-proj"
+import "./components/new-proj";
+import "./components/remove-proj";
